31845009730a8225fe2225cd599ce0007ed06aaf
Stef Bemindt <stef.bemindt01@gmail.com>
2022-12-05 09:39:34 +0100

    Layout changes to item-overview.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
-import { NameFilterPipe } from './pipe/name-filter.pipe';
+import {NameFilterPipe} from './pipe/name-filter.pipe';
 import {ItemOverviewComponent} from "./item-overview/item-overview.component";
-import { CreateItemComponent } from './create-item/create-item.component';
+import {CreateItemComponent} from './create-item/create-item.component';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
@@ -17,13 +18,15 @@ import { CreateItemComponent } from './create-item/create-item.component';
     NameFilterPipe,
     CreateItemComponent,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([]),
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/create-item/create-item.component.ts
+++ src/app/create-item/create-item.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-create-item',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class CreateItemComponent {
 
+  constructor(private location: Location){
+
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
